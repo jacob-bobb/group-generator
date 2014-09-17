33168a68882e9170bde7826889cd7a9f1b83a8ab
Jacob Bobb <jaybobb710@utexas.edu>
2014-09-17 16:45:34 -0500

    added all class so far

---
+++ Grid.java
@@ -0,0 +1,28 @@
+import java.io.File;
+
+
+public class Grid {
+	private int[][][] grid;
+	
+	public Grid(int row, int col, int depth){
+		grid = new int[row][col][depth];
+	}
+	
+	public String toString(){
+		String result = "[";
+		for(int i = 0; i < grid.length; i++){
+			for(int j = 0; j < grid[0].length; j++){
+				for(int k = 0; k < grid[0][0].length; k++){
+					result += grid[i][j][k] + ", ";
+				}
+			}
+		}
+		result += "]";
+		return result;
+	}
+	
+	public void fill(File input){
+		String current = "";
+			
+	}
+}
---
+++ GroupGenerator.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class GroupGenerator implements Students {
+
+	Group[] groups = new Group[6];
+	String[] students = { 
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
